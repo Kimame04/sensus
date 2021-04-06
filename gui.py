1eb979478053aa0d2731e049c194c0f74399e248
Kimame_04 <mendozakyan@gmail.com>
2021-04-06 20:35:49 +0800

    fixed missing text in processed data

--- gui.py
+++ gui.py
@@ -16,40 +16,29 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.title = tk.Label(self, text='Objectivity Detector', font=('Roboto',30))
+        self.title = tk.Label(self, text='Text Objectivity Detector and Text Voice Converter', font=('Roboto',30))
         self.title.grid(row=1,column=0,padx=10,pady=10,sticky=W)
         
         self.field = tk.Text(self)
         self.field.grid(row=2,column=0,padx=10,pady=5)
         
-        self.btn = tk.Button(self, text='Detect Probability', command = self.detectObjectivity)
-        self.btn.grid(row=3,column=0,padx=10,pady=3)
+        self.btn = tk.Button(self, text='Detect Objectivity', command = self.detectObjectivity)
+        self.btn.grid(row=3,column=0,padx=40,pady=3,sticky=W)
+
+        self.btn2 = tk.Button(self, text='Change Text Voice', command = self.generateConverted)
+        self.btn2.grid(row=3,column=0,padx=40,pady=3,sticky=E)
         
-        self.upload = tk.Button(self, text = 'Upload .txt', command = self.uploadTxt)
+        self.upload = tk.Button(self, text = 'Upload .txt File', command = self.uploadTxt)
         self.upload.grid(row=4,column=0,padx=10,pady=3)
         
-        #TODO
-        self.res = tk.Label(self, text='Results go here', font=('Consolas',13))
-        self.res.grid(row=5,column=0,padx=10,pady=10,sticky=W)
-        
-        self.title2 = tk.Label(self, text='Objectivity-Subjectivity Conversion', font=('Consolas',30))
-        self.title2.grid(row=1,column=1,padx=10,pady=10)
-        
-        self.field2 = tk.Text(self)
-        self.field2.grid(row=2,column=1,padx=10,pady=5)
-        
-        self.btn2 = tk.Button(self, text='Convert text', command = self.detectObjectivity)
-        self.btn2.grid(row=3,column=1,padx=10,pady=3)
-        
-        self.upload2 = tk.Button(self, text = 'Upload .txt', command = self.detectObjectivity)
-        self.upload2.grid(row=4,column=1,padx=10,pady=3)
+        self.res = tk.Label(self, text='', font=('Roboto',13))
+        self.res.grid(row=5,column=0,padx=10,pady=10)  
         
         
     def detectObjectivity(self):
         arr = model_direct.predict_proba([self.field.get(1.0,'end')])
-        print(np.array(arr))
-        self.res.config(text = 'Objective: ' + str(round(arr[0][0]*100,2)) + 
-                        '%\nSubjective: ' + str(round(arr[0][1]*100,2)) + '%')
+        self.res.config(text = 'Objective: ' + str(round(arr[0][1]*100,2)) + 
+                        '%\nSubjective: ' + str(round(arr[0][0]*100,2)) + '%')
     def uploadTxt(self):
         tf = filedialog.askopenfilename(
             title="Open Text file", 
@@ -58,6 +47,18 @@ class Application(tk.Frame):
         with open(tf, 'r') as file:
             data = file.read()
         self.field.insert(1.0,data)
+    
+    def generateConverted(self):
+        result = model_direct.predict([self.field.get(1.0,'end')])
+        if result[0] == 0:
+            voice = 'subjective'
+            transformed = 'objective'
+        else: 
+            voice = 'objective'
+            transformed = 'subjective'
+        self.res.config(text = 'Your text is detected to be ' + voice + '. We will therefore transform it to be more '
+                        + transformed + '.')
+        
         
 
 root = tk.Tk()
