1d73381aa79b612f8a2d2d65fec76b0f45349ce0
Kimame_04 <mendozakyan@gmail.com>
2021-04-08 06:11:00 +0800

    fixed gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
 import tkinter as tk 
 import pickle
 import numpy as np
@@ -8,6 +9,7 @@ from nltk.corpus import stopwords
 from nltk.corpus import words
 from nltk.corpus import wordnet
 import re
+from conversion import convert
 
 class Application(tk.Frame):
     global model_direct
@@ -32,11 +34,8 @@ class Application(tk.Frame):
         self.btn = tk.Button(self, text='Detect Objectivity', command = self.detectObjectivity)
         self.btn.grid(row=4,column=0,padx=50,pady=3,sticky=W)
         
-        self.btn2 = tk.Button(self, text='Make More Objective', command = self.makeMoreObjective)
-        self.btn2.grid(row=4,column=0,padx=40,pady=3)
-        
-        self.btn3 = tk.Button(self, text='Make More Subjective', command = self.makeMoreSubjective)
-        self.btn3.grid(row=4,column=0,padx=50,pady=3,sticky=E)
+        self.btn2 = tk.Button(self, text='Objectify/Subjectify (Auto)', command = self.transform)
+        self.btn2.grid(row=4,column=0,padx=40,pady=3,sticky=E)
 
         self.res = tk.Label(self, text='', font=('Roboto',13))
         self.res.grid(row=5,column=0,padx=10,pady=10)  
@@ -54,6 +53,21 @@ class Application(tk.Frame):
         with open(tf, 'r') as file:
             data = file.read()
         self.field.insert(1.0,data)
+        
+    def transform(self):
+        text = self.field.get(1.0,'end')
+        result = convert([text])
+        file = open('subjectified.txt','w')
+        file.write(text)
+        file.close()
+        self.res.config(text = 'Text converted. Please check your file system.')
+        self.alert('Hello','Please wait as we download about 4Gb of data. This is a one-time process.\nThis will take a while')
+    
+    def alert(self,title, message, kind='info', hidemain=True):
+        if kind not in ('error', 'warning', 'info'):
+            raise ValueError('Unsupported alert kind.')
+        show_method = getattr(messagebox, 'show{}'.format(kind))
+        show_method(title, message)
     
     def makeMoreSubjective(self):
         text = self.test(self.field.get(1.0,'end'),True)
@@ -103,18 +117,8 @@ class Application(tk.Frame):
 
 root = tk.Tk()
 root.title('Not a Fact Checker')
-'''
-tabparent = ttk.Notebook(root)
-
-tab1 = ttk.Frame(tabparent)
-tab2 = ttk.Frame(tabparent)
-
-tabparent.add(tab1, text ='Tab 1')
-tabparent.add(tab2, text = 'Tab 2')
-tabparent.pack(expand=1,fill='both')
-'''
         
 app = Application(master=root)
-#app['bg'] = '#545AA7'
 
 root.mainloop()
+
