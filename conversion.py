360ffb012b507896d5c728b682b441d93f769a31
WHY <why.hengyue@gmail.com>
2021-04-08 00:45:01 +0800

    working at last, maybe?

--- conversion.py
+++ conversion.py
@@ -8,6 +8,9 @@ import numpy as np
 from conversion_config import Config
 from model import CtrlGenModel
 
+from nltk import pos_tag, word_tokenize
+
+
 
 def makeVocab(filename):
     word_to_id = {}
@@ -40,7 +43,163 @@ def make_pretrain_embeddings(glove, id_to_word, emb_dim):
     return new_weight
 
 
-def convert(textArr):
+def makeData(srcFile, labelFile, word_to_id, glove, save_file, hidden_size, if_nn_save=False, if_glove=True, if_nn=True,
+             if_shuff=True, if_gender=True):
+    Dataset = []
+    input = {}
+
+    original_text = []
+    original_id = []
+    original_label = []
+    original_length = []
+
+    original_hidden = []
+    original_hidden2 = []
+    temp_tensor = torch.FloatTensor(hidden_size)
+    if if_nn == True:
+        if if_gender == True:
+            original_nn = []
+        else:
+            original_nn = np.load(save_file)
+    else:
+        original_nn = []
+    original_nn_vector = []
+
+    text_line = []
+    max_length = 0
+
+    print('Processing %s & %s ...' % (srcFile, labelFile))
+
+    srcSet = srcFile
+    labelSet = labelFile
+
+    for i in range(len(srcSet)):
+        id_line = []
+        temp_nn_vector = []
+        temp_nn = []
+        temp_hidden = []
+        if if_nn == False and if_nn_save == True:
+            original_nn.append([])
+        srcSet[i] = srcSet[i].strip()
+        if if_nn_save == False:
+            temp_res = pos_tag(word_tokenize(srcSet[i]))
+            for tube in temp_res:
+                if tube[1] == 'NN':
+                    temp_nn.append(tube[0])
+        if if_nn_save == False:
+            original_nn.append(temp_nn)
+        if if_nn == True:
+            for word in original_nn[i]:
+                try:
+                    temp_nn_vector.append(glove.vectors[glove.stoi[word]])
+                except KeyError:
+                    a = 1
+                    print("Unknown words: ", word)
+        original_label.append(int(labelSet[i].strip()))
+        if temp_nn_vector == []:
+            original_hidden.append([int(labelSet[i].strip())] * 300)
+            original_hidden2.append([1 - int(labelSet[i].strip())] * 300)
+        else:
+            temp_tensor = torch.zeros(300)
+            for vector in temp_nn_vector:
+                temp_tensor += vector
+            temp_tensor = temp_tensor / len(temp_nn_vector)
+            original_hidden.append([yi.item() for yi in temp_tensor])
+            original_hidden2.append(original_hidden[i])
+        if i % 5000 == 0:
+            print("now ", i)
+        text_line = ["<BOS>"] + srcSet[i].split() + ["<EOS>"]
+        original_text.append(text_line)
+        original_length.append(len(text_line))
+        original_nn_vector.append(temp_nn_vector)
+
+        if len(text_line) > max_length:
+            max_length = len(text_line)
+        for word in text_line:
+            try:
+                id = word_to_id[word]
+            except KeyError:
+                id = 3
+            id_line.append(id)
+        original_id.append(id_line)
+        if if_nn_save == False:
+            if i % 1000 == 0:
+                print("total: {0} now: {1}".format(len(srcSet), i))
+                print("nn: ", original_nn[i])
+    if if_nn_save == False:
+        save_nn_vector = np.array(original_nn)
+        np.save(save_file, save_nn_vector)
+    if if_shuff:
+        print('... shuffling sentences')
+        perm = torch.randperm(len(original_text))
+        original_id = [original_id[idx] for idx in perm]
+        original_label = [original_label[idx] for idx in perm]
+        original_length = [original_length[idx] for idx in perm]
+        original_text = [original_text[idx] for idx in perm]
+        original_hidden = [original_hidden[idx] for idx in perm]
+        original_hidden2 = [original_hidden2[idx] for idx in perm]
+    if if_nn == True:
+        original_nn = [original_nn[idx] for idx in perm]
+        original_nn_vector = [original_nn_vector[idx] for idx in perm]
+
+    print('... pedding')
+    for i in range(len(original_text)):
+        if original_length[i] < max_length:
+            for j in range(max_length - original_length[i]):
+                original_text[i].append("<PAD>")
+                original_id[i].append(0)
+    Dataset = {"text": original_text, "length": original_length, "text_ids": original_id, "labels": original_label,
+               "nn": original_nn, "nn_vector": original_nn_vector, "hidden": original_hidden,
+               "hidden2": original_hidden2}
+    return Dataset, max_length
+
+
+def makeBatch(Dataset,batch_size):
+    Dataset_total = []
+    text = []
+    length = []
+    text_ids = []
+    labels = []
+    nn = []
+    nn_vector = []
+    hidden = []
+    hidden2 = []
+    unk = []
+    temp = {"text":text,"length":length,"text_ids":text_ids,"labels":labels,"nn":nn,"nn_vector":nn_vector,"unk":unk,"hidden":hidden,"hidden2":hidden2}
+    for i in range(len(Dataset['text'])):
+        temp["text"].append(Dataset['text'][i])
+        temp["length"].append(Dataset['length'][i])
+        temp["text_ids"].append(Dataset['text_ids'][i])
+        temp["labels"].append(Dataset['labels'][i])
+        temp["nn"].append(Dataset["nn"][i])
+        temp["nn_vector"].append(Dataset["nn_vector"][i])
+        temp["hidden"].append(Dataset["hidden"][i])
+        temp["hidden2"].append(Dataset["hidden2"][i])
+        if ((i+1) % batch_size == 0) or (i == len(Dataset['text']) - 1):
+            store = {"text":[row for row in temp['text']],"length":[row for row in temp['length']],"text_ids":[row for row in temp['text_ids']],"labels":[row for row in temp['labels']],"nn":[row for row in temp['nn']],"nn_vector":[row for row in temp['nn_vector']],"hidden":[row for row in temp['hidden']],"hidden2":[row for row in temp['hidden2']]}
+            Dataset_total.append(store)
+            temp['text'].clear()
+            temp['length'].clear()
+            temp['text_ids'].clear()
+            temp['labels'].clear()
+            temp['nn'].clear()
+            temp['nn_vector'].clear()
+            temp['hidden'].clear()
+            temp['hidden2'].clear()
+    for i in range(len(Dataset_total)):
+        raw_inputs = Dataset_total[i]
+        input_text_ids = torch.LongTensor(raw_inputs["text_ids"])
+        input_labels = torch.LongTensor(raw_inputs["labels"])
+        input_length = torch.IntTensor(raw_inputs["length"])
+        input_hidden = torch.FloatTensor(raw_inputs['hidden'])
+        input_hidden2 = torch.FloatTensor(raw_inputs['hidden2'])
+        #input_nn_vector = torch.FloatTensor(raw_inputs["nn_vector"])
+        inputs = {"text":raw_inputs["text"],"labels":input_labels,"length":input_length,"text_ids":input_text_ids,"nn":raw_inputs["nn"],"nn_vector":raw_inputs["nn_vector"],"hidden":input_hidden,"hidden2":input_hidden2}
+        Dataset_total[i] = inputs
+    return Dataset_total
+
+
+def convert(textArr, objective_truth):
     config = Config()
 
     gpu = -1
@@ -64,38 +223,50 @@ def convert(textArr):
 
     glove = torch_vocab.GloVe(name='840B', dim=300, cache="./cached-GloVe/840B.glove")
 
-    #train_nn_file = datapath + "train_nn.npy"
-    #dev_nn_file = datapath + "dev_nn.npy"
+    # train_nn_file = datapath + "train_nn.npy"
+    # dev_nn_file = datapath + "dev_nn.npy"
+    test_nn_file= ""
 
     max_length = 1000
     max_length_dev = 1000
 
+    if objective_truth: t = ["1" for x in range(len(textArr))]
+    else: t = ["0" for x in range(len(textArr))]
+    Dataset_test, _ = makeData(textArr, t, word_to_id, glove, test_nn_file, hidden_size=config.hidden_size, if_nn=False,
+                               if_nn_save=True, if_shuff=False)
+
+    Dataset_test_total = makeBatch(Dataset_test,1)
+
     model = CtrlGenModel(config, vocab_size, len(textArr), weights_matrix)
-    #model = model.cuda()
+    # model = model.cuda()
     if exists("./checkpoint/checkpoint.model"):
-        model.load_state_dict(torch.load("./checkpoint/checkpoint.model",map_location=torch.device('cpu')))
+        model.load_state_dict(torch.load("./checkpoint/checkpoint.model", map_location=torch.device('cpu')))
     else:
         gdown.download("https://drive.google.com/file/d/1gW6taIS1LQJ71qW0y1tlZkgi55EIvNDi/view?usp=sharing",
                        './checkpoint/checkpoint.model', quiet=False)
     # TODO: unlock UI here
 
     output = ""
-    for mn in range(len(textArr)):
-        eval_output, soft_outputs, probs, classes, _ = model(textArr[mn], max_length)
-        result2 = torch.argmax(soft_outputs.transpose(0, 1), 2)
+    for mn in range(len(Dataset_test_total)):
+        eval_output,soft_outputs,probs,classes,_ = model(Dataset_test_total[mn],max_length)
+        result2 = torch.argmax(soft_outputs.transpose(0,1),2)
         p = ""
+        q = ""
         m = ""
-        for j in textArr[mn]:
-            p += " " + j
+        #p = [id_to_word[j.item()] for j in eval_data[79]["text_ids"][i][:]]
+        for j in Dataset_test_total[mn]["text"][0][1:]:
+            if j == "<EOS>":
+                break
+            p = p+" "+j
         for j in result2[0][:]:
             if j == 2:
                 break
-            m = m + " " + id_to_word[j.item()]
-        # f1.write(p+'\n')
-        # f1.write(m+'\n')
+            m = m+ " "+ id_to_word[j.item()]
         output += m + "\n"
     return output
 
 
 if __name__ == "__main__":
-    convert([input("Enter text: ")])
+
+    print(convert([input("Enter text: ")],False))
+    print("done")
