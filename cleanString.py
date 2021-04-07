324cb659d6118c22917232f3978e8b517345ac0b
Kimame_04 <mendozakyan@gmail.com>
2021-04-08 07:57:25 +0800

    fixed cleanstring

--- cleanString.py
+++ cleanString.py
@@ -1,10 +1,15 @@
 import re
 
+def readFromTxt(filename):
+    with open(filename,'r') as file:
+        content = cleanString(file.read())
+    return content
+
 def cleanString(string):
     string = re.sub('[\(\[].*?[\)\]]','',string)
     string = string.replace('.',' .\n')
     return string
 
 if __name__ == "__main__":
-    print(cleanString(input("Enter text: ")))
+    print(readFromTxt('inputtext.txt'))
     print("done")
